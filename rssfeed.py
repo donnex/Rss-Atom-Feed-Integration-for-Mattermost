0f65c748f78e969c02ab35c1b6a266d646e9b87c
Daniel Johansson <daniel.johansson@oderland.se>
2016-05-24 10:39:36 +0200

    Cleanup

--- rssfeed.py
+++ rssfeed.py
@@ -1,18 +1,17 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
-__author__ = 'elpatron@mailbox.org'
-
 import sys
+
 try:
     import html2text
 except ImportError as exc:
-    print('Error: failed to import module. ({}). \nInstall missing modules using '
-                '"sudo pip install -r requirements.txt"'.format(exc))
+    print('Error: failed to import module. ({}). \nInstall missing modules'
+          ' using "sudo pip install -r requirements.txt"'.format(exc))
     sys.exit(0)
 
 
-class RssFeed:
-    def __init__(self, name, url, iconurl, user, channel, showname, showtitle, showdescription, showurl):
+class RssFeed(object):
+    def __init__(self, name, url, iconurl, user, channel, showname, showtitle,
+                 showdescription, showurl):
         self.Name = name
         self.Url = url
         self.Iconurl = iconurl
@@ -32,12 +31,12 @@ class RssFeed:
         h = html2text.HTML2Text()
         h.ignore_links = True
         self.Description = h.handle(self.Description)
-        if self.ShowName == True:
+        if self.ShowName is True:
             text += "_" + self.Name + '_\n'
-        if self.ShowTitle == True:
+        if self.ShowTitle is True:
             text += '*' + self.NewTitle + '*\n'
-        if self.ShowDescription == True:
+        if self.ShowDescription is True:
             text += self.Description + '\n'
-        if self.ShowUrl == True:
+        if self.ShowUrl is True:
             text += self.ArticleUrl
         return text
